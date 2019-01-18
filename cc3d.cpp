9a48ce5dc9c12e396cf010ba7b86eb307970bdeb
William Silversmith <william.silversmith@gmail.com>
2019-01-18 00:35:34 -0500

    chore: bump version to 1.0.2

--- cc3d.cpp
+++ cc3d.cpp
@@ -2476,7 +2476,7 @@ static const char __pyx_k_size[] = "size";
 static const char __pyx_k_step[] = "step";
 static const char __pyx_k_stop[] = "stop";
 static const char __pyx_k_test[] = "__test__";
-static const char __pyx_k_1_0_1[] = "1.0.1";
+static const char __pyx_k_1_0_2[] = "1.0.2";
 static const char __pyx_k_ASCII[] = "ASCII";
 static const char __pyx_k_array[] = "array";
 static const char __pyx_k_class[] = "__class__";
@@ -2590,7 +2590,7 @@ static const char __pyx_k_no_default___reduce___due_to_non[] = "no default __red
 static const char __pyx_k_numpy_core_umath_failed_to_impor[] = "numpy.core.umath failed to import";
 static const char __pyx_k_unable_to_allocate_shape_and_str[] = "unable to allocate shape and strides.";
 static const char __pyx_k_Format_string_allocated_too_shor_2[] = "Format string allocated too short.";
-static PyObject *__pyx_kp_u_1_0_1;
+static PyObject *__pyx_kp_u_1_0_2;
 static PyObject *__pyx_n_s_ASCII;
 static PyObject *__pyx_kp_s_Buffer_view_does_not_expose_stri;
 static PyObject *__pyx_n_u_C;
@@ -20987,7 +20987,7 @@ static struct PyModuleDef __pyx_moduledef = {
 #endif
 
 static __Pyx_StringTabEntry __pyx_string_tab[] = {
-  {&__pyx_kp_u_1_0_1, __pyx_k_1_0_1, sizeof(__pyx_k_1_0_1), 0, 1, 0, 0},
+  {&__pyx_kp_u_1_0_2, __pyx_k_1_0_2, sizeof(__pyx_k_1_0_2), 0, 1, 0, 0},
   {&__pyx_n_s_ASCII, __pyx_k_ASCII, sizeof(__pyx_k_ASCII), 0, 0, 1, 1},
   {&__pyx_kp_s_Buffer_view_does_not_expose_stri, __pyx_k_Buffer_view_does_not_expose_stri, sizeof(__pyx_k_Buffer_view_does_not_expose_stri), 0, 0, 1, 0},
   {&__pyx_n_u_C, __pyx_k_C, sizeof(__pyx_k_C), 0, 1, 0, 1},
@@ -21975,7 +21975,7 @@ if (!__Pyx_RefNanny) {
  * cimport numpy as cnp
  * import numpy as np             # <<<<<<<<<<<<<<
  * 
- * __VERSION__ = '1.0.1'
+ * __VERSION__ = '1.0.2'
  */
   __pyx_t_1 = __Pyx_Import(__pyx_n_s_numpy, 0, 0); if (unlikely(!__pyx_t_1)) __PYX_ERR(0, 21, __pyx_L1_error)
   __Pyx_GOTREF(__pyx_t_1);
@@ -21985,11 +21985,11 @@ if (!__Pyx_RefNanny) {
   /* "cc3d.pyx":23
  * import numpy as np
  * 
- * __VERSION__ = '1.0.1'             # <<<<<<<<<<<<<<
+ * __VERSION__ = '1.0.2'             # <<<<<<<<<<<<<<
  * 
  * cdef extern from "cc3d.hpp" namespace "cc3d":
  */
-  if (PyDict_SetItem(__pyx_d, __pyx_n_s_VERSION, __pyx_kp_u_1_0_1) < 0) __PYX_ERR(0, 23, __pyx_L1_error)
+  if (PyDict_SetItem(__pyx_d, __pyx_n_s_VERSION, __pyx_kp_u_1_0_2) < 0) __PYX_ERR(0, 23, __pyx_L1_error)
 
   /* "cc3d.pyx":32
  *   )
