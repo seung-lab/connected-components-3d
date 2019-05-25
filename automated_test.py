ce71419a92885cde47b0746cb0cb5c34ec978a72
William Silversmith <william.silversmith@gmail.com>
2019-05-24 20:40:14 -0400

    test: compare with scipy

--- automated_test.py
+++ automated_test.py
@@ -364,6 +364,29 @@ def test_max_labels_nonsensical():
   assert np.all(real_labels == zero_labels)
   assert np.all(real_labels == negative_labels)
 
+def test_compare_scipy():
+  import scipy.ndimage.measurements
+  import fastremap
+
+  sx, sy, sz = 256, 256, 256
+  labels = np.random.randint(0,2, (sx,sy,sz), dtype=np.bool)
+
+  structure = [
+    [[1,1,1], [1,1,1], [1,1,1]],
+    [[1,1,1], [1,1,1], [1,1,1]],
+    [[1,1,1], [1,1,1], [1,1,1]]
+  ]
+
+  cc3d_labels = cc3d.connected_components(labels)
+  cc3d_labels, wow = fastremap.renumber(cc3d_labels)
+  scipy_labels, wow = scipy.ndimage.measurements.label(labels, structure=structure)
+
+  print(cc3d_labels)
+  print(scipy_labels)
+
+  assert np.all(cc3d_labels == scipy_labels)
+
+
 # def test_sixty_four_bit():
 #   input_labels = np.ones((1626,1626,1626), dtype=np.uint8)
 #   cc3d.connected_components(input_labels, max_labels=0)  
