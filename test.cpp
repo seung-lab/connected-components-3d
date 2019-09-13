9dd814c08dcb66651980301bf5b4b29efdf9dd8d
fratim <frtim@student.ethz.ch>
2019-09-13 10:47:54 -0400

    edited test file, wrong variable type used

--- test.cpp
+++ test.cpp
@@ -1,6 +1,6 @@
 #include "cc3d.hpp"
 
-void print(uint16_t *input, int sx, int sy, int sz) {
+void print(uint32_t *input, int sx, int sy, int sz) {
 	int i = 0;
 
 	for (int z = 0; z < sz; z++) {
@@ -32,12 +32,12 @@ void print(int *input, int sx, int sy, int sz) {
 
 int main () {
 
-	int *big = new int[512*512*512]();
-	for (int i = 0; i < 512*512*512; i++) {
-		big[i] = 1;
-	}
-
-	cc3d::connected_components3d<int>(big, 512,512,512);	
+	// int *big = new int[512*512*512]();
+	// for (int i = 0; i < 512*512*512; i++) {
+	// 	big[i] = 1;
+	// }
+	//
+	// cc3d::connected_components3d<int>(big, 512,512,512);
 
 
 	// int twod[25] = {
@@ -47,6 +47,28 @@ int main () {
 	// 	0,0,1,0,0,
 	// 	2,2,2,2,2
 	// };
+
+	int td[75] = {
+		1,1,0,0,2,
+		1,1,0,0,0,
+		1,1,1,0,0,
+		0,0,0,1,1,
+		3,2,2,1,1,
+
+		1,1,0,0,2,
+		1,1,0,0,0,
+		1,1,1,0,0,
+		0,0,0,1,1,
+		3,2,2,1,1,
+
+		1,1,0,0,2,
+		1,1,0,0,0,
+		1,1,1,0,0,
+		0,0,0,1,1,
+		3,2,2,1,6
+
+
+	};
 
 	// int threed[27] = {
 	// 	1,1,0,
@@ -62,14 +84,15 @@ int main () {
 	// 	0,0,1
 	// };
 
-	// printf("INPUT\n");
-	// print(twod, 5,5,1);
+	printf("INPUT\n");
+	print(td, 5,5,3);
 
-	// uint16_t *y = cc3d::connected_components3d(twod, 5,5,1);
-	// printf("OUTPUT\n");
-	// print(y, 5,5,1);
+	uint32_t *y = cc3d::connected_components3d(td, 5,5,3,10,6);
+	printf("OUTPUT\n");
+	print(y, 5,5,3);
 
 
+	// compile with: gcc test.cpp -o out_test -lstdc++
 
 	// cc3d::DisjointSet<int> djs;
 	// printf("\nDISJOINT SET\n");
