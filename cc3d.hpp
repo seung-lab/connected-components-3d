7ac7dee4ed057d74bf23bad678d7a4629f70a0b4
Manuel Castro <macastro@princeton.edu>
2019-10-14 13:33:18 -0400

    Feat: find_contact_points between two labels in 3D volume

--- cc3d.hpp
+++ cc3d.hpp
@@ -49,6 +49,8 @@
 #include <cmath>
 #include <cstdio>
 #include <cstdint>
+#include <functional>
+#include <vector>
 
 namespace cc3d {
 
@@ -707,6 +709,90 @@ uint32_t* connected_components3d(
   ) {
   const int64_t voxels = sx * sy * sz;
   return connected_components3d<T>(in_labels, sx, sy, sz, voxels, connectivity);
+}
+
+
+template <typename T>
+std::vector<uint32_t> find_contact_points3d_6(
+    T* data, T label1, T label2,
+    const int64_t sx, const int64_t sy, const int64_t sz
+  ) {
+
+  const int64_t sxy = sx * sy;
+
+  /*
+    Layout of forward pass mask (which faces backwards).
+    N is the current location.
+
+    z = -1     z = 0
+    A B C      J K L   y = -1
+    D E F      M N     y =  0
+    G H I              y = +1
+   -1 0 +1    -1 0   <-- x axis
+  */
+
+  // Z - 1
+  const int64_t E = -sxy;
+
+  // Current Z
+  const int64_t K = -sx;
+  const int64_t M = -1;
+
+  int64_t loc = 0;
+  std::vector<uint32_t> contact_points;
+
+  auto add_contact_point = [&] (T point1_label, int64_t xcoord, int64_t ycoord,
+    int64_t zcoord,  T point2_label, int64_t xcoord2,
+    int64_t ycoord2, int64_t zcoord2
+  ) {
+    if (point1_label == label1) {
+      uint32_t coords[6] = {xcoord, ycoord, zcoord, xcoord2, ycoord2, zcoord2};
+      contact_points.insert(contact_points.end(), coords, std::end(coords));
+    } else {
+      uint32_t coords[6] = {xcoord2, ycoord2, zcoord2, xcoord, ycoord, zcoord};
+      contact_points.insert(contact_points.end(), coords, std::end(coords));
+    }
+  };
+
+
+  for (int64_t z = 0; z < sz; z++) {
+    for (int64_t y = 0; y < sy; y++) {
+      for (int64_t x = 0; x < sx; x++) {
+        loc = x + sx * (y + sy * z);
+        const T cur = data[loc];
+        if (cur == label1 || cur == label2) {
+          const T test_value = (cur == label1) ? label2 : label1;
+          if (x > 0 && data[loc + M] == test_value) {
+            add_contact_point(cur, x, y, z, test_value, x-1, y, z);
+          }
+          if (y > 0 && data[loc + K] == test_value) {
+            add_contact_point(cur, x, y, z, test_value, x, y-1, z);
+          }
+          if (z > 0 && data[loc + E] == test_value) {
+            add_contact_point(cur, x, y, z, test_value, x, y, z-1);
+          }
+        }
+      }
+    }
+  }
+
+  return contact_points;
+}
+
+template <typename T>
+std::vector<uint32_t> find_contact_points3d(
+    T* data, T label1, T label2,
+    const int64_t sx, const int64_t sy, const int64_t sz,
+    const int64_t connectivity
+  ) {
+  if (connectivity == 6) {
+    return find_contact_points3d_6<T>(
+      data, label1, label2, sx, sy, sz
+    );
+  }
+  else {
+    throw "Currently, only 6 3D connectivity is supported.";
+  }
 }
 
 };
--- cc3d.pyx
+++ cc3d.pyx
@@ -32,7 +32,7 @@ from libc.stdint cimport (
   int8_t, int16_t, int32_t, int64_t,
   uint8_t, uint16_t, uint32_t, uint64_t,
 )
-from cpython cimport array 
+from cpython cimport array
 import array
 import sys
 
@@ -49,6 +49,11 @@ cdef extern from "cc3d.hpp" namespace "cc3d":
     int64_t max_labels, int64_t connectivity,
     uint32_t* out_labels
   )
+  cdef vector[uint32_t] find_contact_points3d[T](
+    T* in_labels, T label1, T label2,
+    int64_t sx, int64_t sy, int64_t sz,
+    int64_t connectivity
+  )
 
 ctypedef fused INTEGER:
   uint8_t
@@ -63,6 +68,35 @@ ctypedef fused INTEGER:
 class DimensionError(Exception):
   """The array has the wrong number of dimensions."""
   pass
+
+def _order_data(data):
+  order = 'F' if data.flags['F_CONTIGUOUS'] else 'C'
+
+  while len(data.shape) < 3:
+    if order == 'C':
+      data = data[np.newaxis, ...]
+    else: # F
+      data = data[..., np.newaxis ]
+
+  if not data.flags['C_CONTIGUOUS'] and not data.flags['F_CONTIGUOUS']:
+    data = np.copy(data, order=order)
+
+  shape = list(data.shape)
+
+  # The default C order of 4D numpy arrays is (channel, depth, row, col)
+  # col is the fastest changing index in the underlying buffer.
+  # fpzip expects an XYZC orientation in the array, namely nx changes most rapidly.
+  # Since in this case, col is the most rapidly changing index,
+  # the inputs to fpzip should be X=col, Y=row, Z=depth, F=channel
+  # If the order is F, the default array shape is fine.
+  if order == 'C':
+    shape.reverse()
+
+  cdef int sx = shape[0]
+  cdef int sy = shape[1]
+  cdef int sz = shape[2]
+
+  return data, order, sx, sy, sz
 
 def connected_components(data, int64_t max_labels=-1, int64_t connectivity=26):
   """
@@ -92,31 +126,7 @@ def connected_components(data, int64_t max_labels=-1, int64_t connectivity=26):
   if data.size == 0:
     return np.zeros(shape=(0,), dtype=np.uint32)
 
-  order = 'F' if data.flags['F_CONTIGUOUS'] else 'C'
-
-  while len(data.shape) < 3:
-    if order == 'C':
-      data = data[np.newaxis, ...]
-    else: # F
-      data = data[..., np.newaxis ]
-
-  if not data.flags['C_CONTIGUOUS'] and not data.flags['F_CONTIGUOUS']:
-    data = np.copy(data, order=order)
-
-  shape = list(data.shape)
-
-  # The default C order of 4D numpy arrays is (channel, depth, row, col)
-  # col is the fastest changing index in the underlying buffer. 
-  # fpzip expects an XYZC orientation in the array, namely nx changes most rapidly. 
-  # Since in this case, col is the most rapidly changing index, 
-  # the inputs to fpzip should be X=col, Y=row, Z=depth, F=channel
-  # If the order is F, the default array shape is fine.
-  if order == 'C':
-    shape.reverse()
-
-  cdef int sx = shape[0]
-  cdef int sy = shape[1]
-  cdef int sz = shape[2]
+  data, order, sx, sy, sz = _order_data(data)
 
   cdef uint8_t[:,:,:] arr_memview8u
   cdef uint16_t[:,:,:] arr_memview16u
@@ -310,6 +320,94 @@ cdef tuple neighbors(
       (x < sx - 1 and y <  sy - 1 and z > 0 and labels[x + 1, y + 1, z - 1]),
     )
 
+def contact_points(data, label1, label2):
+  """
+  ndarray contact_points(data, label1, label2)
 
+  Find all contact points between two labels in
+  a 3D labeled volume.
 
+  Required:
+    data: Input labels in a 2D or 3D numpy array.
+    label1, label2: Two labels we are interested in
 
+  Returns: Array of pairs of coordinates that repesent
+    contact points between the two labels. The first
+    coordinate in the pair is a label1 coordinate, and
+    the second a label2 one.
+  """
+  dims = len(data.shape)
+  if dims not in (1,2,3):
+    raise DimensionError("Only 1D, 2D, and 3D arrays supported. Got: " + str(dims))
+
+  if data.size == 0:
+    return np.zeros(shape=(0,), dtype=np.uint32)
+
+  data, _, sx, sy, sz = _order_data(data)
+
+  cdef uint8_t[:,:,:] arr_memview8u
+  cdef uint16_t[:,:,:] arr_memview16u
+  cdef uint32_t[:,:,:] arr_memview32u
+  cdef uint64_t[:,:,:] arr_memview64u
+  cdef int8_t[:,:,:] arr_memview8
+  cdef int16_t[:,:,:] arr_memview16
+  cdef int32_t[:,:,:] arr_memview32
+  cdef int64_t[:,:,:] arr_memview64
+
+  dtype = data.dtype
+  # TODO: Support 18 and 26 connectivity
+  connectivity = 6
+
+  if dtype == np.uint64:
+    arr_memview64u = data
+    contact_points = find_contact_points3d[uint64_t](
+      &arr_memview64u[0,0,0], label1, label2,
+      sx, sy, sz, connectivity
+    )
+  elif dtype == np.uint32:
+    arr_memview32u = data
+    contact_points = find_contact_points3d[uint32_t](
+      &arr_memview32u[0,0,0], label1, label2,
+      sx, sy, sz, connectivity
+    )
+  elif dtype == np.uint16:
+    arr_memview16u = data
+    contact_points = find_contact_points3d[uint16_t](
+      &arr_memview16u[0,0,0], label1, label2,
+      sx, sy, sz, connectivity
+    )
+  elif dtype in (np.uint8, np.bool):
+    arr_memview8u = data.astype(np.uint8)
+    contact_points = find_contact_points3d[uint8_t](
+      &arr_memview8u[0,0,0], label1, label2,
+      sx, sy, sz, connectivity
+    )
+  elif dtype == np.int64:
+    arr_memview64 = data
+    contact_points = find_contact_points3d[int64_t](
+      &arr_memview64[0,0,0], label1, label2,
+      sx, sy, sz, connectivity
+    )
+  elif dtype == np.int32:
+    arr_memview32 = data
+    contact_points = find_contact_points3d[int32_t](
+      &arr_memview32[0,0,0], label1, label2,
+      sx, sy, sz, connectivity
+    )
+  elif dtype == np.int16:
+    arr_memview16 = data
+    contact_points = find_contact_points3d[int16_t](
+      &arr_memview16[0,0,0], label1, label2,
+      sx, sy, sz, connectivity
+    )
+  elif dtype == np.int8:
+    arr_memview8 = data
+    contact_points = find_contact_points3d[int8_t](
+      &arr_memview8[0,0,0], label1, label2,
+      sx, sy, sz, connectivity
+    )
+  else:
+    raise TypeError("Type {} not currently supported.".format(dtype))
+
+  number_contact_sites = int(len(contact_points) / 6)
+  return np.reshape(contact_points, newshape=(number_contact_sites, 2, 3))
