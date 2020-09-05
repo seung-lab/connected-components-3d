9980ba2024bff69ab4940c146a33c453ac6c113a
William Silversmith <william.silversmith@gmail.com>
2020-09-05 19:46:25 -0400

    chore(linux): added streamlined build process for linux

---
+++ build_linux.sh
@@ -0,0 +1,5 @@
+#!/usr/local/bin/bash
+docker build . -f manylinux1.Dockerfile --tag seunglab/cc3d:manylinux1
+docker build . -f manylinux2010.Dockerfile --tag seunglab/cc3d:manylinux2010
+docker run -v $PWD/dist:/output seunglab/cc3d:manylinux1 /bin/bash -c "cp -r wheelhouse/* /output"
+docker run -v $PWD/dist:/output seunglab/cc3d:manylinux2010 /bin/bash -c "cp -r wheelhouse/* /output"
