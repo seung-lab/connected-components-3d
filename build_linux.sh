d5653d2aaee9371d451729ad421df3ecb030e04e
William Silversmith <william.silversmith@gmail.com>
2020-09-06 03:42:34 -0400

    chore(manylinux2014): adapt build_linux.sh script

--- build_linux.sh
+++ build_linux.sh
@@ -3,3 +3,4 @@ docker build . -f manylinux1.Dockerfile --tag seunglab/cc3d:manylinux1
 docker build . -f manylinux2010.Dockerfile --tag seunglab/cc3d:manylinux2010
 docker run -v $PWD/dist:/output seunglab/cc3d:manylinux1 /bin/bash -c "cp -r wheelhouse/* /output"
 docker run -v $PWD/dist:/output seunglab/cc3d:manylinux2010 /bin/bash -c "cp -r wheelhouse/* /output"
+docker run -v $PWD/dist:/output seunglab/cc3d:manylinux2014 /bin/bash -c "cp -r wheelhouse/* /output"
