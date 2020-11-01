e253dd9a944b748bbe5c0e35fa91ea6967bb310e
William Silversmith <william.silversmith@gmail.com>
2020-11-01 18:47:10 -0500

    release(2.0.0): zeroth_pass, automatic dtype, return_N, drop sparse, voxel_connectivity_graph

--- cc3d.cpp
+++ cc3d.cpp
@@ -2759,6 +2759,7 @@ static const char __pyx_k_step[] = "step";
 static const char __pyx_k_stop[] = "stop";
 static const char __pyx_k_test[] = "__test__";
 static const char __pyx_k_view[] = "view";
+static const char __pyx_k_2_0_0[] = "2.0.0";
 static const char __pyx_k_ASCII[] = "ASCII";
 static const char __pyx_k_array[] = "array";
 static const char __pyx_k_class[] = "__class__";
@@ -2779,7 +2780,6 @@ static const char __pyx_k_start[] = "start";
 static const char __pyx_k_strip[] = "strip";
 static const char __pyx_k_uint8[] = "uint8";
 static const char __pyx_k_zeros[] = "zeros";
-static const char __pyx_k_1_14_0[] = "1.14.0";
 static const char __pyx_k_encode[] = "encode";
 static const char __pyx_k_format[] = "format";
 static const char __pyx_k_graph8[] = "graph8";
@@ -2910,7 +2910,7 @@ static const char __pyx_k_no_default___reduce___due_to_non[] = "no default __red
 static const char __pyx_k_numpy_core_umath_failed_to_impor[] = "numpy.core.umath failed to import";
 static const char __pyx_k_unable_to_allocate_shape_and_str[] = "unable to allocate shape and strides.";
 static const char __pyx_k_Only_6_18_and_26_connectivities_2[] = "Only 6, 18, and 26 connectivities are supported. Got: ";
-static PyObject *__pyx_kp_u_1_14_0;
+static PyObject *__pyx_kp_u_2_0_0;
 static PyObject *__pyx_n_s_ASCII;
 static PyObject *__pyx_kp_s_Buffer_view_does_not_expose_stri;
 static PyObject *__pyx_n_u_C;
@@ -29175,7 +29175,7 @@ static struct PyModuleDef __pyx_moduledef = {
 #endif
 
 static __Pyx_StringTabEntry __pyx_string_tab[] = {
-  {&__pyx_kp_u_1_14_0, __pyx_k_1_14_0, sizeof(__pyx_k_1_14_0), 0, 1, 0, 0},
+  {&__pyx_kp_u_2_0_0, __pyx_k_2_0_0, sizeof(__pyx_k_2_0_0), 0, 1, 0, 0},
   {&__pyx_n_s_ASCII, __pyx_k_ASCII, sizeof(__pyx_k_ASCII), 0, 0, 1, 1},
   {&__pyx_kp_s_Buffer_view_does_not_expose_stri, __pyx_k_Buffer_view_does_not_expose_stri, sizeof(__pyx_k_Buffer_view_does_not_expose_stri), 0, 0, 1, 0},
   {&__pyx_n_u_C, __pyx_k_C, sizeof(__pyx_k_C), 0, 1, 0, 1},
@@ -30215,7 +30215,7 @@ if (!__Pyx_RefNanny) {
  * cimport numpy as cnp
  * import numpy as np             # <<<<<<<<<<<<<<
  * 
- * __VERSION__ = '1.14.0'
+ * __VERSION__ = '2.0.0'
  */
   __pyx_t_2 = __Pyx_Import(__pyx_n_s_numpy, 0, 0); if (unlikely(!__pyx_t_2)) __PYX_ERR(0, 45, __pyx_L1_error)
   __Pyx_GOTREF(__pyx_t_2);
@@ -30225,11 +30225,11 @@ if (!__Pyx_RefNanny) {
   /* "cc3d.pyx":47
  * import numpy as np
  * 
- * __VERSION__ = '1.14.0'             # <<<<<<<<<<<<<<
+ * __VERSION__ = '2.0.0'             # <<<<<<<<<<<<<<
  * 
  * cdef extern from "cc3d.hpp" namespace "cc3d":
  */
-  if (PyDict_SetItem(__pyx_d, __pyx_n_s_VERSION, __pyx_kp_u_1_14_0) < 0) __PYX_ERR(0, 47, __pyx_L1_error)
+  if (PyDict_SetItem(__pyx_d, __pyx_n_s_VERSION, __pyx_kp_u_2_0_0) < 0) __PYX_ERR(0, 47, __pyx_L1_error)
 
   /* "cc3d.pyx":82
  *   int64_t
--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ if sys.platform == 'darwin':
 
 setuptools.setup(
   name="connected-components-3d",
-  version="1.14.0",
+  version="2.0.0",
   setup_requires=['pbr', 'numpy'],
   install_requires=['numpy'],
   extras_require={
