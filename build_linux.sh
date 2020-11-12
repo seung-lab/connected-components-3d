24cf8c9c49f5318c03b4a2bf30312b9443417f02
William Silversmith <william.silversmith@gmail.com>
2020-11-12 01:40:02 -0500

    chore: make build_linux.sh more generic

--- build_linux.sh
+++ build_linux.sh
@@ -1,7 +1,9 @@
 #!/usr/local/bin/bash
-docker build . -f manylinux1.Dockerfile --tag seunglab/cc3d:manylinux1
-docker build . -f manylinux2010.Dockerfile --tag seunglab/cc3d:manylinux2010
-docker build . -f manylinux2014.Dockerfile --tag seunglab/cc3d:manylinux2014
-docker run -v $PWD/dist:/output seunglab/cc3d:manylinux1 /bin/bash -c "cp -r wheelhouse/* /output"
-docker run -v $PWD/dist:/output seunglab/cc3d:manylinux2010 /bin/bash -c "cp -r wheelhouse/* /output"
-docker run -v $PWD/dist:/output seunglab/cc3d:manylinux2014 /bin/bash -c "cp -r wheelhouse/* /output"
+container_name=$(basename $(dirname $(realpath $0)))
+echo "Building seunglab/$container_name"
+docker build . -f manylinux1.Dockerfile --tag "seunglab/$container_name:manylinux1"
+docker build . -f manylinux2010.Dockerfile --tag "seunglab/$container_name:manylinux2010"
+docker build . -f manylinux2014.Dockerfile --tag "seunglab/$container_name:manylinux2014"
+docker run -v $PWD/dist:/output "seunglab/$container_name:manylinux1" /bin/bash -c "cp -r wheelhouse/* /output"
+docker run -v $PWD/dist:/output "seunglab/$container_name:manylinux2010" /bin/bash -c "cp -r wheelhouse/* /output"
+docker run -v $PWD/dist:/output "seunglab/$container_name:manylinux2014" /bin/bash -c "cp -r wheelhouse/* /output"
