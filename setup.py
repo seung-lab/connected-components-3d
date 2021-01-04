c8c489e0055def04158a5547d3463780e0a13ae6
William Silversmith <william.silversmith@gmail.com>
2021-01-03 19:59:54 -0500

    release(3.1.1): fixes bug in 26-connected labeling

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ if sys.platform == 'darwin':
 
 setuptools.setup(
   name="connected-components-3d",
-  version="3.1.0",
+  version="3.1.1",
   setup_requires=['pbr', 'numpy'],
   install_requires=['numpy'],
   extras_require={
