bc0967f45112a458e4d218ec61fee1e1c3ff9bf2
William Silversmith <william.silversmith@gmail.com>
2021-04-01 22:54:10 -0400

    release(3.2.0): faster performance for very simple images
    Also includes a fix for images where every voxel is different.

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ if sys.platform == 'darwin':
 
 setuptools.setup(
   name="connected-components-3d",
-  version="3.1.2",
+  version="3.2.0",
   setup_requires=['pbr', 'numpy'],
   install_requires=['numpy'],
   extras_require={
