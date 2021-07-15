9ed7d02ba989290573b571c1812572d54bffab86
William Silversmith <william.silversmith@gmail.com>
2021-07-15 11:48:40 -0400

    release(3.2.1): raise cpp errors, fixes bug for C-order arrays

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ if sys.platform == 'darwin':
 
 setuptools.setup(
   name="connected-components-3d",
-  version="3.2.0",
+  version="3.2.1",
   setup_requires=['pbr', 'numpy'],
   install_requires=['numpy'],
   python_requires="~=3.6", # >= 3.6 < 4.0
