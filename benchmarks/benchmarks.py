1f08f40420d0dd498c16ebc07f7af21e1765e51c
William Silversmith <william.silversmith@gmail.com>
2021-09-08 21:15:07 -0400

    chore: cleanup benchmarks.py

--- benchmarks/benchmarks.py
+++ benchmarks/benchmarks.py
@@ -5,6 +5,35 @@ import numpy as np
 import fastremap
 import time
 
+def prettyprint(tm, voxels):
+  print("{:.2f} sec. {:.2f} MVx/sec".format(tm, voxels / tm / (1000*1000)))
+
+def summary(times, voxels):
+  times = times[1:]
+  mean = sum(times) / float(len(times))
+
+  print("mean:")
+  prettyprint(mean, voxels)
+  print("stddev: %.2f" % np.std(times))
+
+structures = {
+  6: [
+    [[0,0,0], [0,1,0], [0,0,0]],
+    [[0,1,0], [1,1,1], [0,1,0]],
+    [[0,0,0], [0,1,0], [0,0,0]]
+  ],
+  18: [
+    [[0,1,0], [1,1,1], [0,1,0]],
+    [[1,1,1], [1,1,1], [1,1,1]],
+    [[0,1,0], [1,1,1], [0,1,0]]
+  ],
+  26: [
+    [[1,1,1], [1,1,1], [1,1,1]],
+    [[1,1,1], [1,1,1], [1,1,1]],
+    [[1,1,1], [1,1,1], [1,1,1]]
+  ]
+}
+
 def cc3d_mutlilabel_extraction(labels):
   res, N = cc3d.connected_components(labels, return_N=True)
   for label, extracted in tqdm(cc3d.each(labels, binary=True, in_place=True), total=N):
@@ -26,40 +55,37 @@ def ndimage_mutlilabel_extraction(labels):
     for ccid in tqdm(range(1,N+1)):
       extracted = (res == ccid)
 
-def pinky40subvol():
-  from cloudvolume import CloudVolume
-  return CloudVolume('gs://neuroglancer/wms/skeletonization/pinky40subvol', 
-    mip=2, progress=True, cache=True)[:,:,:][...,0]
+def test_binary_image_processing(labels, connectivity=26):
+  voxels = labels.size
+  times = []
 
+  for label, img in tqdm(cc3d.each(labels, binary=True, in_place=True)):
+    s = time.time()
+    cc3d.connected_components(img)
+    # scipy.ndimage.measurements.label(labels, structure=structures[connectivity])
+    e = time.time()
+    dt = e-s
+    times.append(dt)
+    prettyprint(dt, voxels)
+  summary(times, voxels)
 
+def test_multilabel_speed(labels, connectivity=26):
+  voxels = labels.size
+  times = []
 
-labels = pinky40subvol()
-cc3d_mutlilabel_extraction(labels)
+  for i in range(10):
+    s = time.time()
+    cc3d.connected_components(labels, connectivity=connectivity)
+    e = time.time()
+    dt = e-s
+    times.append(dt)
+    prettyprint(dt, voxels)
+  summary(times, voxels)
+
+labels = np.load("connectomics.npy")
+# cc3d_mutlilabel_extraction(labels)
 # ndimage_mutlilabel_extraction(labels)
 
-# voxels = labels.size
-# times = []
-# # for label, img in tqdm(cc3d.each(labels, binary=False, in_place=True)):
-# #   s = time.time()
-# #   cc3d.connected_components(img)
-# #   # scipy.ndimage.measurements.label(labels, structure=structure)
-# #   e = time.time()
-# #   dt = e-s
-# #   times.append(dt)
-# #   prettyprint(dt, voxels)
+test_multilabel_speed(labels)
 
-# for i in range(10):
-#   s = time.time()
-#   cc3d.connected_components(labels)#, connectivity=26)
-#   # scipy.ndimage.measurements.label(labels)
-#   e = time.time()
-#   dt = e-s
-#   times.append(dt)
-#   prettyprint(dt, voxels)
 
-# times = times[1:]
-# mean = sum(times) / float(len(times))
-
-# print("mean:")
-# prettyprint(mean, voxels)
-# print("stddev: %.2f" % np.std(times))
