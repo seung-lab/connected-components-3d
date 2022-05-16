01a51a1cdf822629fb4940fb1b24e1892f475512
William Silversmith <william.silversmith@gmail.com>
2022-05-16 17:39:06 -0400

    test: update benchmarks with scikit image

--- benchmarks/benchmarks.py
+++ benchmarks/benchmarks.py
@@ -1,5 +1,6 @@
 import cc3d
 from tqdm import tqdm
+import skimage.measure
 import scipy.ndimage.measurements
 import numpy as np
 import fastremap
@@ -34,12 +35,21 @@ structures = {
   ]
 }
 
+def scikit_image_multilabel_extraction(labels):
+  s = time.time()
+  res, N = skimage.measure.label(labels, return_num=True)
+  for segid in tqdm(range(1, N+1)):
+    extracted = (res == segid)
+  print(time.time() - s, "seconds")
+
 def cc3d_mutlilabel_extraction(labels):
+  s = time.time()
   res, N = cc3d.connected_components(labels, return_N=True)
   for label, extracted in tqdm(cc3d.each(labels, binary=True, in_place=True), total=N):
     pass
   # for segid in tqdm(range(1, N+1)):
   #   extracted = (res == segid)
+  print(time.time() - s, "seconds")
 
 def ndimage_mutlilabel_extraction(labels):
   s = [
@@ -102,12 +112,14 @@ def test_continuous_speed(labels, connectivity=8):
   _run(upper)
 
 
-# cc3d_mutlilabel_extraction(labels)
+labels = np.load("connectomics.npy")
+scikit_image_multilabel_extraction(labels)
+cc3d_mutlilabel_extraction(labels)
 # ndimage_mutlilabel_extraction(labels)
 # test_multilabel_speed(labels)
 
-labels = np.load("Misc_pollen.npy").astype(np.float32)
-test_continuous_speed(labels)
+# labels = np.load("Misc_pollen.npy").astype(np.float32)
+# test_continuous_speed(labels)
 
 
 
