7564bc40b43e39add0af5ff70b3afcb857b488f3
William Silversmith <william.silversmith@gmail.com>
2022-05-16 15:48:24 -0400

    release(3.10.0): cc3d.contacts new function

--- cc3d.pyx
+++ cc3d.pyx
@@ -49,7 +49,7 @@ cimport numpy as cnp
 import numpy as np
 import time
 
-__VERSION__ = '3.9.0'
+__VERSION__ = '3.10.0'
 
 cdef extern from "cc3d.hpp" namespace "cc3d":
   cdef size_t estimate_provisional_label_count[T](
