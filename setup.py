b3c46c8762fa019634a1a16b59afd2e0e263abe2
William Silversmith <william.silversmith@gmail.com>
2024-04-04 23:53:41 -0400

    release(3.13.0): adds periodic_boundary feature

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ if sys.platform == 'darwin':
 
 setuptools.setup(
   name="connected-components-3d",
-  version="3.12.6",
+  version="3.13.0",
   setup_requires=['pbr', 'numpy', 'cython'],
   install_requires=['numpy'],
   python_requires=">=3.7,<4.0",
