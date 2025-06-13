b5a27ed618b8de4f6d85565f2f88fbeaeae937c8
William Silversmith <william.silversmith@gmail.com>
2025-06-13 18:11:27 -0400

    install: remove excess license information causing build breakage

--- setup.py
+++ setup.py
@@ -72,7 +72,6 @@ setuptools.setup(
   classifiers=[
     "Intended Audience :: Developers",
     "Development Status :: 5 - Production/Stable",
-    "License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)",
     "Programming Language :: Python",
     "Programming Language :: Python :: 3",
     "Programming Language :: Python :: 3.9",
