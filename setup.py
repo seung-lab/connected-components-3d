346faa477966168eb29fb25312074e83f9c9108a
William Silversmith <william.silversmith@gmail.com>
2025-10-03 17:30:50 -0400

    release(3.25.0): fix and speedup for connected_components_stack

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ if sys.platform == 'darwin':
 
 setuptools.setup(
   name="connected-components-3d",
-  version="3.24.0",
+  version="3.25.0",
   setup_requires=['pbr', 'numpy', 'cython'],
   install_requires=['numpy'],
   python_requires=">=3.9,<4.0",
