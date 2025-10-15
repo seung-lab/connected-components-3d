0ab366feb6eb8a66fed8c82124479c0fe001ec0b
William Silversmith <william.silversmith@gmail.com>
2025-10-15 00:04:58 -0400

    release(3.25.1): faster 2d contact calculation, correct surface area

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ if sys.platform == 'darwin':
 
 setuptools.setup(
   name="connected-components-3d",
-  version="3.25.0",
+  version="3.25.1",
   setup_requires=['pbr', 'numpy', 'cython'],
   install_requires=['numpy'],
   python_requires=">=3.9,<4.0",
